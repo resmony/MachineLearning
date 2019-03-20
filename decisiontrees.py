f4b2567dd99963e9491d12e26704024c0cb9750b
resmony <munizresmony@live.com>
2019-03-19 22:26:20 -0300

    decision tree applied

--- decisiontrees.py
+++ decisiontrees.py
@@ -50,3 +50,37 @@ plt.xticks(range(X_train.shape[1]),
 plt.xlim([-1, X_train.shape[1]])
 plt.tight_layout()
 plt.show()
+#%%
+print(forest.predict_proba([[14.23, 1.71, 2.43, 15.6, 127, 2.80, 3.06, 0.28, 2.29, 5.64, 1.04, 3.92, 1065]]))
+
+#%%
+from sklearn.tree import DecisionTreeClassifier
+
+feat_labels = df_wine.columns[1:]
+
+tree = DecisionTreeClassifier()
+
+tree.fit(X_train, y_train)
+importances = tree.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, 
+                            feat_labels[indices[f]], 
+                            importances[indices[f]]))
+
+plt.title('Feature Importances')
+plt.bar(range(X_train.shape[1]), 
+        importances[indices],
+        color='lightblue', 
+        align='center')
+
+plt.xticks(range(X_train.shape[1]), 
+           feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()
+#%%
+test_predict = [[14.23, 1.71, 2.43, 15.6, 127, 2.80, 3.06, 0.28, 2.29, 5.64, 1.04, 3.92, 1065]]
+print(tree.predict(test_predict))
